ad9cb55e1b375b7a1a893304a40c9b2772603e4b
baeian <ddoory1103@gmail.com>
2023-04-19 15:39:47 +0900

    DangerSalesWorker

--- C++수업/20230419/Employee.cpp
+++ C++수업/20230419/Employee.cpp
@@ -14,6 +14,7 @@ public:
 	}
 	virtual int GetPay() const
 	{
+		return 0;
 	}
 	void ShowYourName() const
 	{
@@ -31,12 +32,11 @@ class PermanentWorker : public Employee
 {
 private:
 	int salary;
-	string typeOfEmployment;
+
 public:
 	PermanentWorker(string name, int money)
-			: Employee(name), salary(money),typeOfEmployment("PermanentWorker")
+			: Employee(name), salary(money)
 	{
-		
 	}
 	virtual int GetPay() const
 	{
@@ -63,7 +63,7 @@ public:
 	{
 		return workHour * payPerHour;
 	}
-	void ShowSalaryInfo() const override
+	virtual void ShowSalaryInfo() const override
 	{
 		ShowYourName();
 		cout << "pay per hour: " << payPerHour << endl
@@ -75,7 +75,7 @@ public:
 
 class SalesWorker : public PermanentWorker
 {
-private:
+protected:
 	int salesRecord;
 	double bonusRatio;
 
@@ -91,13 +91,59 @@ public:
 	{
 		return PermanentWorker::GetPay() + (int)(salesRecord * bonusRatio);
 	}
-	void ShowSalaryInfo() const override
+	virtual void ShowSalaryInfo() const override
 	{
 		ShowYourName();
 		cout << "default salary " << PermanentWorker::GetPay() << endl
 				 << "sales result: " << salesRecord << endl
 				 << "bonus ratio: " << bonusRatio << endl
-				 << "salary: " << GetPay() << endl
+				 << "total salary: " << GetPay() << endl
+				 << endl;
+	}
+};
+
+class DangerSalesWorker : public SalesWorker
+{
+private:
+	char riskType; // 위험 노출도
+
+public:
+	DangerSalesWorker(string name, int money, double ratio, char riskType) : SalesWorker(name, money, ratio), riskType(riskType)
+	{
+	}
+
+	int GetRiskPay() const
+	{
+		switch (riskType)
+		{
+		case 'A':
+			return (int)(SalesWorker::salesRecord * SalesWorker::bonusRatio * 0.3);
+			break;
+		case 'B':
+			return (int)(SalesWorker::salesRecord * SalesWorker::bonusRatio * 0.2);
+			break;
+		case 'C':
+			return (int)(SalesWorker::salesRecord * SalesWorker::bonusRatio * 0.1);
+			break;
+		default:
+			return 0;
+			break;
+		}
+	}
+
+	virtual int GetPay() const override
+	{
+		return SalesWorker::GetPay() + GetRiskPay();
+	}
+	virtual void ShowSalaryInfo() const override
+	{
+		ShowYourName();
+		cout << "default salary " << PermanentWorker::GetPay() << endl
+				 << "sales result: " << salesRecord << endl
+				 << "bonus ratio: " << bonusRatio << endl
+				 << "risk type: " << riskType << endl
+				 << "risk pay: " << GetRiskPay() << endl
+				 << "total salary: " << GetPay() << endl
 				 << endl;
 	}
 };
@@ -154,8 +200,18 @@ int main(void)
 
 	// 영업직 등록
 	SalesWorker *seller = new SalesWorker("Hong", 1000, 0.1);
-	seller->AddSalesResult(6000); // 영업실적 7000
+	seller->AddSalesResult(7000); // 영업실적 7000
 	handler.AddEmployee(seller);
+
+	// 위험지역영업직 등록
+	SalesWorker *dangerSeller1 = new DangerSalesWorker("Park", 2000, 0.1, 'A');
+	dangerSeller1->AddSalesResult(8000); // 영업실적 8000
+	handler.AddEmployee(dangerSeller1);
+
+	// 위험지역영업직 등록
+	SalesWorker *dangerSeller2 = new DangerSalesWorker("Park", 1000, 0.2, 'A');
+	dangerSeller2->AddSalesResult(4000); // 영업실적 4000
+	handler.AddEmployee(dangerSeller2);
 
 	// 이번 달에 지불해야 할 급여의 정보
 	handler.ShowAllSalaryInfo();
